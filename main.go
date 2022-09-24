c4ccd83192578315c142709be8af7e06c1f8e7fa
PatrikDark97 <darkpatrik1@gmail.com>
2022-09-24 11:57:00 +0530

    add food_id count from log file basic

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const LogFile = "orders.log"
+
+var CountMap = make(map[string]int)
+
+func CountFoodItem(fs *bufio.Scanner) {
+	for fs.Scan() {
+		food := strings.Split(fs.Text(), ",")[1]
+		CountMap[food] = CountMap[food] + 1
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	readFile, err := os.Open(LogFile)
+	defer readFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	CountFoodItem(fileScanner)
+
+	fmt.Println(CountMap)
 }
